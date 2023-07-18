2316523cf2a4262e953e4b6420df5e19d9d45b75
iammorganevans <111830280+iammorganevans@users.noreply.github.com>
2023-07-17 23:13:04 -0400

    Add files via upload

--- finalProject/projectJS.js
+++ finalProject/projectJS.js
@@ -3,5 +3,10 @@ function makeMealPlan() {
     window.open(mealPlanPage, "_blank");
 }
 
-const submit = document.getElementById("submit")
+const submit = document.getElementById("submit");
 submit.addEventListener("click", makeMealPlan);
+
+function emailValidation(email) {
+   let regex = /^[a-zA-Z0-9._%+-]+@[a-zA-Z0-9.-]+\.[a-zA-Z]{2,}$/.test(email);
+}
+
